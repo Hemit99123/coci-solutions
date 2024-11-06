efb51a09bbe4347dc32003b9614cc12619310786
Hemit Patel <patelvhemit@gmail.com>
2024-11-05 23:14:04 -0500

    Update dominos.py

--- contest-1/2009/dominos.py
+++ contest-1/2009/dominos.py
@@ -1,9 +1,12 @@
 limit = int(input())
 
 counter = 0
-total_sum = 0  # Renamed to avoid overwriting the built-in sum function
+total_sum = 0 
 
 for row in range(limit + 1):
+    # by adding this range which uses an ever increasing row, you can limiting the range of iterations by 1 each time 
+    # this way not all combinations are accounted for just the ones that are needed (i.e 8,2 wouldnt be accounted for because 2,8 would have added that sum)
+    
     for column in range(row, limit + 1):
         total_sum += row + column
 
